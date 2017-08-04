f748f2be6aef20e8f94565095bc60d9ea514d9af
Torey Hickman <torey@toreyhickman.com>
2017-08-04 16:37:15 -0400

    refactor: Move bookshelf changer to separate component

--- src/book.js
+++ src/book.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import BookshelfChanger from './bookshelfchanger'
 
 class Book extends Component {
   static PropTypes = {
@@ -15,15 +16,7 @@ class Book extends Component {
       <div className="book">
         <div className="book-top">
           <div className="book-cover" style={{width: 128, height: 193, backgroundImage: `url(${thumbnail})`}}></div>
-          <div className="book-shelf-changer">
-            <select>
-              <option value="none" disabled>Move to...</option>
-              <option value="currentlyReading">Currently Reading</option>
-              <option value="wantToRead">Want to Read</option>
-              <option value="read">Read</option>
-              <option value="none">None</option>
-            </select>
-          </div>
+          <BookshelfChanger />
         </div>
         <div className="book-title">{title}</div>
         <div className="book-authors">{author}</div>
