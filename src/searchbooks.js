57c89249bb6a2b5043346f7b862f0ed6363f1b4e
Torey Hickman <torey@toreyhickman.com>
2017-08-06 15:54:41 -0400

    refactor: Move search input to separate component

--- src/searchbooks.js
+++ src/searchbooks.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import SearchInput from './searchinput'
 
 class SearchBooks extends Component {
   render() {
@@ -7,18 +8,7 @@ class SearchBooks extends Component {
       <div className="search-books">
         <div className="search-books-bar">
           <Link to="/" className="close-search">Close</Link>
-          <div className="search-books-input-wrapper">
-            {/*
-              NOTES: The search from BooksAPI is limited to a particular set of search terms.
-              You can find these search terms here:
-              https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-              However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-              you don't find a specific author or title. Every search is limited by search terms.
-            */}
-            <input type="text" placeholder="Search by title or author"/>
-
-          </div>
+          <SearchInput />
         </div>
         <div className="search-books-results">
           <ol className="books-grid"></ol>
