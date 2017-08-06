7480f046fa6da4e369fb93aee6f647ab7ee5a01e
Torey Hickman <torey@toreyhickman.com>
2017-08-06 16:00:49 -0400

    refactor: Move search results to separate component

--- src/searchbooks.js
+++ src/searchbooks.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react'
 import SearchBar from './searchbar'
+import SearchResults from './searchresults'
 
 class SearchBooks extends Component {
   render() {
     return (
       <div className="search-books">
         <SearchBar />
-        <div className="search-books-results">
-          <ol className="books-grid"></ol>
-        </div>
+        <SearchResults />
       </div>
     )
   }
